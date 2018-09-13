7af56d39e27e2cd455f06a111eaae17e4ca89701
wilmar <wil.pac510@gmail.com>
2018-09-12 19:13:53 -0500

    backup database

--- src/main/java/com/winki/demo/dao/UserRepository.java
+++ src/main/java/com/winki/demo/dao/UserRepository.java
@@ -1,7 +1,5 @@
 package com.winki.demo.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.winki.demo.model.User;
---
+++ src/main/resources/backup.sql
@@ -0,0 +1,49 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.5.5-10.1.16-MariaDB : Database - usersdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `hibernate_sequence` */
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `hibernate_sequence` */
+
+insert  into `hibernate_sequence`(`next_val`) values (5);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `first_surname` varchar(255) NOT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `second_name` varchar(255) DEFAULT NULL,
+  `second_surname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`address`,`first_name`,`first_surname`,`phone`,`second_name`,`second_surname`) values (1,'lucio cabañas','wilmar','postman','876238473','poki','Python'),(2,'Corregidora,72550','user1.1','pac','1598756','postman','Python'),(4,'Lucio Cabañas 1','luis','Cardenaz',NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
